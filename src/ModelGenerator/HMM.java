7271e5eb07f906bff9c1c16c0ad5a431f1cc21e5
FATESAIKOU <qzsecftbhhhh@google.com>
2017-08-03 13:45:46 +0800

    HMM model generation Complete

--- src/ModelGenerator/HMM.java
+++ src/ModelGenerator/HMM.java
@@ -2,33 +2,83 @@ import java.io.*;
 import java.util.*;
 import org.json.*;
 
-
 public class HMM
 {
     private State trans_count;
-    private HashMap<String, HashMap<String, Integer>> emmit_count;
+    private State emmit_count;
 
     public HMM()
     {
-        // init
+        trans_count = new State("_T");
+        emmit_count = new State("_E");
     }
 
     public HMM(String model_file)
+        throws IOException, JSONException
     {
-        // Generate JSON object
-        //    trans_count = new State(json['trans']);
-        //    emmit_count = new EmmitProb(json['trans']);
+        // Create File Reader
+        File file = new File(model_file);
+        FileInputStream fis = new FileInputStream(file);
+
+        // Get File Content
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+
+        // Create State
+        JSONObject hmm_mdl = new JSONObject(new String(data, "UTF-8"));
+
+        // Load Model
+        trans_count = new State(hmm_mdl.getJSONObject("trans"));
+        emmit_count = new State(hmm_mdl.getJSONObject("emmit"));
     }
 
-    public void genTrans(String trans_seq_file)
+    public void genCounts(String trans_seq_file, String emmit_seq_file) throws IOException
     {
-        // load sequence file
-        //    trans_count.walk(String[] path);
+        // Get File Scanner
+        Scanner trans_seq = getSeq(trans_seq_file);
+        Scanner emmit_seq = getSeq(emmit_seq_file);
+
+        // Initialize State Path
+        List<String> states = new LinkedList<String>();
+        states.add("START");
+        states.add("START");
+
+        String now_state, now_emmit;
+        while (trans_seq.hasNext()) {
+            now_state = trans_seq.next();
+
+            // Add To Emmit Count
+            if (emmit_seq.hasNext()) {
+                now_emmit = emmit_seq.next();
+                emmit_count.walk(new String[]{}, 0);
+                emmit_count.walk(new String[] {now_state, now_emmit}, 0);
+            }
+            
+            // Add To State Count
+            states.add(now_state);
+            trans_count.walk(new String[]{}, 0);
+            trans_count.walk(new String[] {states.get(0)}, 0);
+            trans_count.walk(new String[] {states.get(0), states.get(1)}, 0);
+            trans_count.walk(new String[] {states.get(0), states.get(1), states.get(2)}, 0);
+            states.remove(0);
+        }
     }
 
-    public void genEmmit(String trans_seq_file, String emmit_seq_file)
+    public JSONObject toJSON() throws JSONException 
     {
-        // load sequence file
-        //    emmit.addCount(String stateA, String e_stateA);
+        JSONObject mdl = new JSONObject();
+
+        mdl.put("Trans", trans_count.toJSON());
+        mdl.put("Emmit", emmit_count.toJSON());
+
+        return mdl;
+    }
+
+    private Scanner getSeq(String seq_file) throws FileNotFoundException
+    {
+        File seq_src = new File(seq_file);
+
+        return new Scanner(seq_src);
     }
 }
--- src/ModelGenerator/HMMTester.java
+++ src/ModelGenerator/HMMTester.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import org.json.*;
+
 public class HMMTester
 {
     private HMM hmm;
@@ -8,7 +11,7 @@ public class HMMTester
         String emmit_seq_file = args[1];
         String state_seq_file = args[2];
         String model_name = args[3];
-
+        System.out.println("Init");
 
         // Load or Build Model
         if (args.length > 4) {
@@ -17,16 +20,21 @@ public class HMMTester
             hmmt = new HMMTester();
             hmmt.process(emmit_seq_file, state_seq_file);
         }
+        System.out.println("Load Model End");
+
 
         // Test Model
         hmmt.initTest();
         hmmt.test(emmit_seq_file, state_seq_file);
+        System.out.println("Testing Model");
 
         // Report
         hmmt.report();
+        System.out.println("Reporting");
 
         // Save Model
         hmmt.save(model_name);
+        System.out.println("Saving");
     }
 
     public HMMTester()
@@ -36,13 +44,22 @@ public class HMMTester
 
     public HMMTester(String model_file)
     {
-        hmm = new HMM(model_file);
+        try {
+            hmm = new HMM(model_file);
+        } catch (Exception e) {
+            System.out.println("Something went wrong :)");
+            e.printStackTrace();
+        }
     }
 
     public void process(String emmit_seq_file, String state_seq_file)
     {
-        hmm.genTrans(state_seq_file);
-        hmm.genEmmit(state_seq_file, emmit_seq_file);
+        try {
+            hmm.genCounts(state_seq_file, emmit_seq_file);
+        } catch (IOException e) {
+            System.out.println("Something went wrong :)");
+            e.printStackTrace();
+        }
     }
 
     public void initTest()
@@ -62,6 +79,13 @@ public class HMMTester
 
     public void save(String model_name)
     {
-        // HMM dump
+        try {
+            PrintWriter out = new PrintWriter(model_name, "UTF-8");
+            out.print(hmm.toJSON().toString());
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Something went wrong :)");
+            e.printStackTrace();
+        }
     }
 }
